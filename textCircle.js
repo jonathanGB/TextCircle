d8f4d31c30b7e03d09808f0879763d389199a7c7
Jonathan Guillotte-Blouin <jonathan.guillotte.blouin@gmail.com>
2015-12-29 02:40:20 -0500

    Week 2 complete

--- textCircle.js
+++ textCircle.js
@@ -1,4 +1,6 @@
 this.Documents = new Mongo.Collection('documents');
+EditingUsers = new Mongo.Collection('editingUsers');
+
 
 if (Meteor.isClient) {
   Template.editor.helpers({
@@ -13,10 +15,38 @@ if (Meteor.isClient) {
 
     config: function() {
       return function(editor) {
+        editor.setOption('lineNumbers', true);
+        editor.setOption('theme', 'railscasts');
+
         editor.on('change', function(cm_editor, info) {
           $('iframe#viewer_iframe').contents().find('html').html(cm_editor.getValue());
+          Meteor.call('addEditingUser');
         });
       };
+    }
+  });
+
+  Template.editingUsers.helpers({
+    users: function() {
+      var doc, eUsers, users;
+      doc = Documents.findOne();
+
+      if (!doc) return;
+
+      eUsers = EditingUsers.findOne({docid: doc._id});
+
+      if (!eUsers) return;
+
+      users = new Array();
+      for (var user_id in eUsers.users)
+        users.push(eUsers.users[user_id]);
+
+      console.log(users);
+      return users;
+    },
+
+    getEmail: function(emails) {
+      return emails[0].address;
     }
   });
 }
@@ -29,3 +59,28 @@ if (Meteor.isServer) {
       Documents.insert({title: "My New Document"});
   });
 }
+
+Meteor.methods({
+  addEditingUser: function() {
+    var doc, user, eUsers;
+    doc = Documents.findOne();
+
+    if (!doc || !this.userId) return;
+
+    user = Meteor.user();
+
+    eUsers = EditingUsers.findOne({docid: doc._id});
+
+    if (!eUsers) {
+        eUsers = {
+          docid: doc._id,
+          users: {}
+        };
+    }
+
+    user.lastEdit = new Date();
+    eUsers.users[this.userId] = user;
+
+    EditingUsers.upsert({_id: eUsers._id}, eUsers);
+  }
+});
